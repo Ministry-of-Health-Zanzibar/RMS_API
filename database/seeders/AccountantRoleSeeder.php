8bdb4bfc1611cc4daac91b979b4c0825f6bdc1ba
HassanSaidHamad <hassantwinn5said@gmail.com>
2025-04-19 11:53:33 +0300

    Commit changes

---
+++ database/seeders/AccountantRoleSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AccountantRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = Role::create(['name' => 'ROLE ACCOUNTANT']);
+
+        $permissionNames = [
+            'Create Source',
+            'Update Source',
+            'Delete Source',
+            'View Source',
+
+            'Create Source Type',
+            'Update Source Type',
+            'Delete Source Type',
+            'View Source Type',
+
+            'Create Category',
+            'Update Category',
+            'Delete Category',
+            'View Category',
+
+            'Create Document Type',
+            'Update Document Type',
+            'Delete Document Type',
+            'View Document Type',
+
+            'Create Document Form',
+            'Update Document Form',
+            'Delete Document Form',
+            'View Document Form',
+        ];
+        $permissions = Permission::whereIn('name', $permissionNames)->get();
+
+        $role->syncPermissions($permissions);
+    }
+
+}
