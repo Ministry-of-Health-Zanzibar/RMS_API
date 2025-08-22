acd488a519f547126a281c7faa6864bea61498aa
Mr. Yussuf <yusoufomar@gmail.com>
2025-08-22 10:38:49 +0300

    Add monthly bill ID as refference in payment

--- database/migrations/2025_04_18_120405_create_payments_table.php
+++ database/migrations/2025_04_18_120405_create_payments_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->bigIncrements('payment_id');
-            $table->unsignedBigInteger('bill_id');
+            $table->unsignedBigInteger('monthly_bill_id');
             $table->decimal('amount_paid', 12, 2);
             $table->string('payment_method')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('bill_id')->references('bill_id')->on('bills');
+            $table->foreign('monthly_bill_id')->references('monthly_bill_id')->on('monthly_bills');
         });
     }
 
