26bb1422564a23395303cf99bd6c0bbe263fa880
Mr. Yussuf <yusoufomar@gmail.com>
2025-10-08 14:08:07 +0300

    Remove Accountant

--- database/seeders/user_info.php
+++ database/seeders/user_info.php
@@ -42,75 +42,7 @@ class user_info extends Seeder
         $admin->syncPermissions($allPermissions);
         $admin->syncRoles([$adminRole]);
 
-        // 2. Accountant user
-        $accountant = User::firstOrCreate(
-            ['email' => 'accountant@mohz.go.tz'],
-            [
-                'first_name' => 'Accountant',
-                'middle_name' => 'Accountant',
-                'last_name' => 'Accountant',
-                'address' => 'Kilimani',
-                'gender' => 'Male',
-                'phone_no' => '0777000003',
-                'date_of_birth' => '1990-10-30',
-                'password' => bcrypt('accountant@123'),
-                'created_by' => $admin->id,
-            ]
-        );
-
-        $accountantRole = Role::firstOrCreate(
-            ['name' => 'ROLE ACCOUNTANT', 'guard_name' => 'web'],
-            ['created_by' => $admin->id]
-        );
-
-        $permissions = [
-            'Accountant Module',
-            'Create Source',
-            'Update Source',
-            'Delete Source',
-            'View Source',
-            'Create Source Type',
-            'Update Source Type',
-            'Delete Source Type',
-            'View Source Type',
-            'Create Category',
-            'Update Category',
-            'Delete Category',
-            'View Category',
-            'Create Document Type',
-            'Update Document Type',
-            'Delete Document Type',
-            'View Document Type',
-            'Create Document Form',
-            'Update Document Form',
-            'Delete Document Form',
-            'View Document Form',
-            'View Report',
-            'View Dashboard',
-            'View Permission',
-            'Create Role',
-            'Update Role',
-            'Delete Role',
-            'View Role',
-            'Create User',
-            'Update User',
-            'Delete User',
-            'View User',
-        ];
-
-        $accountantPermissions = collect();
-        foreach ($permissions as $permName) {
-            $permission = Permission::firstOrCreate(
-                ['name' => $permName, 'guard_name' => 'web']
-            );
-            $accountantPermissions->push($permission->name);
-        }
-
-        $accountantRole->syncPermissions($accountantPermissions);
-        $accountant->syncPermissions($accountantPermissions);
-        $accountant->syncRoles([$accountantRole]);
-
-        // 3. DG user
+        // 2. DG user
         $dg = User::firstOrCreate(
             ['email' => 'dg@mohz.go.tz'],
             [
@@ -136,16 +68,19 @@ class user_info extends Seeder
             'Update ReferralLetter',
             'Delete ReferralLetter',
             'View ReferralLetter',
+            
             'Referral Module',
             'Create Referral',
             'Update Referral',
             'Delete Referral',
             'View Referral',
+
             'Bill Module',
             'Create Bill',
             'Update Bill',
             'Delete Bill',
             'View Bill',
+
             'Payment Module',
             'Create Payment',
             'Update Payment',
