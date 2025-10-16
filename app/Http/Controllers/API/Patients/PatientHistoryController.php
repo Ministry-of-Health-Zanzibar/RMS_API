c5423ec36eacc5df86b484e6ffd04178177a5002
Mr. Yussuf <itsmryussuf@gmail.com>
2025-10-16 17:05:58 +0300

    add referral when adding patient history

--- app/Http/Controllers/API/Patients/PatientHistoryController.php
+++ app/Http/Controllers/API/Patients/PatientHistoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\Patients;
 use App\Http\Controllers\Controller;
 use App\Models\PatientHistory;
 use App\Models\Diagnosis;
+use App\Models\Referral;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
