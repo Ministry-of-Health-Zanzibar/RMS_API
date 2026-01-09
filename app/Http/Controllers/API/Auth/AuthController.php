d92c1cf3b59a3561ee2be4346b6bb42b54c08c13
Mr. Yussuf <itsmryussuf@gmail.com>
2026-01-09 14:13:04 +0300

    Add Hospital to dashboard of Hospital User

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -17,6 +17,7 @@ use DB;
 
 class AuthController extends Controller
 {
+
     public function login(Request $request)
     {
         $data = Validator::make($request->all(),[
@@ -28,38 +29,44 @@ class AuthController extends Controller
             return response()->json($data->errors());
         }
 
-        // $user = Auth::attempt($request->only('email','password'));
         if(!Auth::attempt($request->only('email','password'))){
-
             Helper::sendError('Email Or Password is incorrect !!!');
-            // return response()
-            //     ->json(['message' => 'Unauthorized','status'=> 401]);
-        }
-        else{
-
+        } else {
             $roles = [];
             $permissions = [];
-
+            $hospitalInfo = null; // default null if not hospital user
 
             foreach(auth()->user()->roles as $print){
-                $rolePermissions = Permission::join('role_has_permissions', 'role_has_permissions.permission_id', 'permissions.id')->where('role_has_permissions.role_id',$print->id)->get();
-                array_push($roles, array(
+                $rolePermissions = Permission::join('role_has_permissions', 'role_has_permissions.permission_id', 'permissions.id')
+                                    ->where('role_has_permissions.role_id',$print->id)->get();
+
+                array_push($roles, [
                     "id" => $print->id,
                     "name" => $print->name,
-                ));
+                ]);
 
-                //all permission from each role assigned
-                foreach($rolePermissions as $print){
-                    array_push($permissions, array(
-                        "id" => $print->id,
-                        "name" => $print->name
-                    ));
+                // all permissions from each role assigned
+                foreach($rolePermissions as $perm){
+                    array_push($permissions, [
+                        "id" => $perm->id,
+                        "name" => $perm->name
+                    ]);
+                }
+
+                // Check if user has HOSPITAL USER role
+                if($print->name === 'ROLE HOSPITAL USER'){
+                    $hospitalInfo = auth()->user()->hospitals->map(function($hospital){
+                        return [
+                            'hospital_id'   => $hospital->hospital_id,
+                            'hospital_name' => $hospital->hospital_name, // use correct column name from hospitals table
+                            'hospital_role' => $hospital->pivot->role,
+                        ];
+                    });
                 }
             }
 
-            //return new UserResource(auth()->user());
             $token = auth()->user()->createToken('auth_token')->plainTextToken;
-            $data = array(
+            $data = [
                 'user_id' => auth()->user()->id,
                 'email' => auth()->user()->email,
                 'full_name' => auth()->user()->first_name." ". auth()->user()->middle_name." ". auth()->user()->last_name,
@@ -67,8 +74,9 @@ class AuthController extends Controller
                 'statusCode' => 200,
                 'token' => $token,
                 'roles' => $roles,
-                'permissions' => $permissions
-            );
+                'permissions' => $permissions,
+                'hospital_info' => $hospitalInfo // added hospital info here
+            ];
 
             return response()->json(['data'=>$data]);
         }
